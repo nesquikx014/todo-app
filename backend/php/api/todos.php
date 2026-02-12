b6584cd6a9b45d7bd2e97c9f7260637deee18cc4
nesquikx014 <nesquikx014@users.noreply.github.com>
2026-02-13 00:43:06 +0100

    todos.php gewijgd les3

--- backend/php/api/todos.php
+++ backend/php/api/todos.php
@@ -1,4 +1,8 @@
+
+
 <?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
 $conn = new mysqli("db", "root", "rootpassword", "todo_app");
 
